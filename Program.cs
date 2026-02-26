2e47ceca2b4c65971083d20482e27beca8119190
codeine322 <qwertywow553@gmail.com>
2026-02-26 12:12:10 +0700

    Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,10 @@ namespace GuessNumber
 {
     internal class Program
     {
-        // Точка входа в программу, управляет игровым циклом и выводит статистику
+        /// <summary>
+        /// Точка входа в программу. Управляет игровым циклом и выводит итоговую статистику.
+        /// </summary>
+        /// <param name="args">Аргументы командной строки.</param>
         static void Main(string[] args)
         {
             int min = 0;
@@ -16,18 +19,30 @@ namespace GuessNumber
             do
             {
                 int couneter = PlayGame(rnd, ref min, ref max, ref count, ref countGame);
-                Console.WriteLine("Do you want play game?");
-                answer = Convert.ToChar(Console.Read());
+                Console.WriteLine("Do you want play game? (Y/N)");
+                // Читаем строку и берем первый символ, чтобы избежать проблем с буфером ввода
+                string input = Console.ReadLine() ?? "N";
+                answer = input.Length > 0 ? char.ToUpper(input[0]) : 'N';
             }
             while (answer == 'Y');
-            Console.WriteLine($"min = {min} max = {max} avg = {(double)count / countGame}");
+            
+            if (countGame > 0)
+                Console.WriteLine($"min = {min} max = {max} avg = {(double)count / countGame}");
         }
 
-        // Метод для проведения одной игры, обновляет статистику
+        /// <summary>
+        /// Проводит одну игровую сессию по угадыванию числа.
+        /// </summary>
+        /// <param name="rnd">Экземпляр генератора случайных чисел.</param>
+        /// <param name="min">Минимальное количество попыток за все игры (обновляется по ссылке).</param>
+        /// <param name="max">Максимальное количество попыток за все игры (обновляется по ссылке).</param>
+        /// <param name="count">Общее количество попыток за все игры (обновляется по ссылке).</param>
+        /// <param name="countGame">Общее количество проведенных игр (обновляется по ссылке).</param>
+        /// <returns>Количество попыток, затраченных в текущей сессии.</returns>
         static int PlayGame(Random rnd, ref int min, ref int max, ref int count, ref int countGame)
         {
             int couneter = 0;
-            int number = rnd.Next(1, 100);
+            int number = rnd.Next(1, 101); // 101, чтобы включить число 100
             Console.WriteLine("Try guess number?");
             while (true)
             {
@@ -51,7 +66,11 @@ namespace GuessNumber
             return couneter;
         }
 
-        // Метод для чтения и проверки введенного пользователем числа
+        /// <summary>
+        /// Считывает число пользователя из консоли с валидацией диапазона [1;100].
+        /// Предоставляет 3 попытки на корректный ввод, иначе завершает программу.
+        /// </summary>
+        /// <returns>Валидное целое число, введенное пользователем.</returns>
         static int ReadUserNumber()
         {
             int userNumber = 0;
@@ -61,16 +80,18 @@ namespace GuessNumber
             {
                 if (!int.TryParse(Console.ReadLine(), out userNumber)
                     || userNumber > 100 || userNumber < 1)
-                    Console.WriteLine("Input number from [1;100]");
-                else
-                    break;
-                if (i == 2)
                 {
-                    Console.WriteLine("You are stupid");
-                    Environment.Exit(0);
+                    if (i < 2) Console.WriteLine("Invalid input. Try again. Input number from [1;100]");
+                }
+                else
+                {
+                    return userNumber;
                 }
             }
-            return userNumber;
+            
+            Console.WriteLine("You are stupid");
+            Environment.Exit(0);
+            return 0;
         }
     }
 }
